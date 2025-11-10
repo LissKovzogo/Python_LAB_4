09b7be64258c190bf6d516d978db4f9b41bf9d39
LissKovzogo <Silika0Clea@yandex.ru>
2025-11-10 17:20:11 +0300

    Исправления

--- Indiv/ind_2.py
+++ Indiv/ind_2.py
@@ -15,12 +15,7 @@ if __name__ == '__main__':
     if abs(min_a) > max_a:
         ind = a.index(min_a)
 
-    ind_s = -1
-
-    for i in a:
-        if i > 0:
-            ind_s = a.index(i)
-            break
+    ind_s = next((i for i, num in enumerate(a) if num > 0))
 
     sum_a  = sum(a[ind_s+1::])
     a = sorted(a, key=lambda x: (0 if s <= x <= b else 1, x))
--- Indiv/ind_3.py
+++ Indiv/ind_3.py
@@ -11,8 +11,6 @@ if __name__ == '__main__':
     )
 
     sorted_teams = sorted(teams, key=lambda x: x[1], reverse=True)
-    print(sorted_teams)
-    for i, (team, point) in enumerate(sorted_teams):
-        # print(f"Место: {i+1} {team} {point}")
-        print(f"Место: {i + 1} {sorted_teams[i][0]} {sorted_teams[i][1]}")
-        # print(f"Место: {i + 1} {sorted_teams[i]} ")
+
+    for i, (team, point) in enumerate(sorted_teams,1):
+        print(f"Место: {i} {team} {point}")
